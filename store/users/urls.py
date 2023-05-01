0feca5cd2dab557e0b375f69c8c05ebf6446c26d
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-01 16:47:03 +0300

    Update urls.py

--- store/users/urls.py
+++ store/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
-from . views import SignUpView,SignInView,profile
+from . views import SignUpView, SignInView, profile
+
 app_name = 'users'
 
 urlpatterns = [
