86780afb128f402e35828b0c8d34a5212d32cc42
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-02 15:56:31 +0300

    updated Admin and template

--- store/products/admin.py
+++ store/products/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from products.models import ProductCategory, Product
+from products.models import ProductCategory, Product, Basket
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('image', 'name', ('price', 'quantity'), 'category', 'description')
+    # readonly_fields = ()
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
