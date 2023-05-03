442c4cfe256756cc45acc11b5382d2b223777a9d
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-03 16:30:57 +0300

    view form update and adding Mixin

--- store/products/models.py
+++ store/products/models.py
@@ -7,6 +7,12 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=True, blank=True)
 
+
+    class Meta:
+        verbose_name = ('Категории')
+        verbose_name_plural = ('Категории')
+
+
     def __str__(self):
         return f'{self.name} {self.description}'
 
@@ -18,6 +24,10 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images', null=True, blank=True)
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ('Продукт')
+        verbose_name_plural = ('Продукты')
 
     def __str__(self):
         return f'Продукт - {self.name} | Категория - {self.category}'
--- store/products/views.py
+++ store/products/views.py
@@ -1,29 +1,31 @@
-from django.shortcuts import render
 from products.models import ProductCategory, Product, Basket
-from users.models import User
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from store.common.views import TitleMixin
 
 
-def index(request):
-    return render(request, 'products/index.html', context={
-        'title': 'My store',
-
-    })
+class IndexView(TitleMixin, TemplateView):
+    template_name = 'products/index.html'
+    title = 'My store'
 
 
-def products(request, category_id=None, page_number=1):
-    products = Product.objects.filter(category=category_id) if category_id else Product.objects.all()
-    per_page = 3
-    paginator = Paginator(products,per_page)
-    products_paginator = paginator.page(page_number)
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
 
-    return render(request, 'products/products.html', context={
-        'title': 'Store - Каталог',
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator,
-    })
+    def get_queryset(self):
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductsListView, self).get_context_data()
+        context['title'] = 'Store - Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
 
 
 @login_required
