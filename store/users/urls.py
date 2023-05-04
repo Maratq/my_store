3d347a1beb7e7b6eecf2fa4e46eb553ba63888b9
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-04 11:45:26 +0300

    added EmailVerificationView Urls and template

--- store/users/urls.py
+++ store/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import UserRegisterView, UserLoginView, UserProfileView
+from .views import UserRegisterView, UserLoginView, UserProfileView, EmailVerificationView
 from django.contrib.auth.views import LogoutView
 
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
     path('logout/',LogoutView.as_view(),name='logout'),
+    path('verify/<str:email>/<uuid:code>/',EmailVerificationView.as_view(),name='email_verification'),
 ]
