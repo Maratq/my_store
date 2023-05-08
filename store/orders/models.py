8ae5c67b1a97c22e7bc45146c72a1fd05b3620c3
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-08 17:53:31 +0300

    created OrderModels and migrate

---
+++ store/orders/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.conf import settings
+
+class Order(models.Model):
+    CREATED = 0
+    PAID = 1
+    ON_WAY = 2
+    DELIVERED = 3
+    STATUSES = (
+        (CREATED, 'Создан'),
+        (PAID, 'Оплачен'),
+        (ON_WAY, 'В пути'),
+        (DELIVERED, 'Доставлен'),
+    )
+
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=256)
+    address = models.CharField(max_length=256)
+    basket_history = models.JSONField(default=dict)
+    initiator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.PositiveSmallIntegerField(default=CREATED, choices=STATUSES)
+
+    def __str__(self):
+        return f'Order # {self.id} {self.first_name} {self.last_name}'
