50e686fe4964778de4a3d6285749e6852196ff10
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-12 19:54:22 +0300

    added orders path

--- store/orders/urls.py
+++ store/orders/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from orders.views import OrderCreateView
+from .views import (CanceledTemplateView, OrderCreateView,SuccessTemplateView)
 
 app_name = 'orders'
 
 urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name='order_create'),
+   #path('', OrderListView.as_view(), name='orders_list'),
+   # path('order/<int:pk>/', OrderDetailView.as_view(), name='order'),
+    path('order-success/', SuccessTemplateView.as_view(), name='order_success'),
+    path('order-canceled/', CanceledTemplateView.as_view(), name='order_canceled'),
 ]
