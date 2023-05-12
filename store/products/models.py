af57efe9133baa6e9a3725ec50a730d76e7d7f4f
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-12 19:52:35 +0300

    added stripe and fix

--- store/products/models.py
+++ store/products/models.py
@@ -1,36 +1,39 @@
+import stripe
+from django.conf import settings
 from django.db import models
 
 from users.models import User
 
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
 
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
-
     class Meta:
-        verbose_name = ('Категории')
-        verbose_name_plural = ('Категории')
-
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
-        return f'{self.name} {self.description}'
+        return self.name
 
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images', null=True, blank=True)
+    stripe_product_price_id = models.CharField(max_length=128, null=True, blank=True)
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = ('Продукт')
-        verbose_name_plural = ('Продукты')
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
 
     def __str__(self):
-        return f'Продукт - {self.name} | Категория - {self.category}'
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
 
 
 class BasketQuerySet(models.QuerySet):
@@ -39,6 +42,16 @@ class BasketQuerySet(models.QuerySet):
 
     def total_quantity(self):
         return sum(basket.quantity for basket in self)
+
+    def stripe_products(self):
+        line_items = []
+        for basket in self:
+            item = {
+                'price': basket.product.stripe_product_price_id,
+                'quantity': basket.quantity,
+            }
+            line_items.append(item)
+        return line_items
 
 
 class Basket(models.Model):
@@ -50,7 +63,7 @@ class Basket(models.Model):
     objects = BasketQuerySet.as_manager()
 
     def __str__(self):
-        return f'Корзина для {self.username} | Продукт {self.product.name} '
+        return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
 
     def sum(self):
         return self.product.price * self.quantity
