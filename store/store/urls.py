d85b7ca8641dab3ab0d6e121f5458dbf725944f3
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-19 19:52:34 +0300

    added api urls

--- store/store/urls.py
+++ store/store/urls.py
@@ -1,22 +1,8 @@
-"""store URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from orders.views import stripe_webhook_view
 from products.views import IndexView
@@ -25,9 +11,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('products/', include('products.urls'), name='products'),
+    path('accounts/', include('allauth.urls')),
     path('users/', include('users.urls'), name='users'),
     path('orders/', include('orders.urls'), name='order'),
     path('webhook/stripe/', stripe_webhook_view, name='stripe_webhook'),
+    path('api/', include('api.urls'), name='api'),
+    path('api-token-auth/', obtain_auth_token)
 ]
 
 if settings.DEBUG:
