41cf4424a4dfa4f9c322ccaa75aa860dcf499872
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-19 20:02:31 +0300

    Basket fields for admin

--- store/products/admin.py
+++ store/products/admin.py
@@ -8,9 +8,14 @@ admin.site.register(ProductCategory)
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'quantity', 'category')
-    fields = ('image', 'name', ('price', 'quantity'),'stripe_product_price_id', 'category', 'description')
-    # readonly_fields = ()
+    fields = ('image', 'name', 'description', ('price', 'quantity'), 'stripe_product_price_id', 'category')
+    readonly_fields = ('description',)
     search_fields = ('name',)
-    ordering = ('name',)
+    ordering = ('-name',)
 
 
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
