8ef2483cef20ca923d25a031efd001099a81ecd4
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-19 19:47:14 +0300

    Update urls.py

--- store/users/urls.py
+++ store/users/urls.py
@@ -2,8 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import (EmailVerificationView, UserLoginView, UserProfileView,
-                    UserRegisterView)
+from users.views import (EmailVerificationView, UserLoginView, UserProfileView,
+                         UserRegisterView)
 
 app_name = 'users'
 
@@ -11,6 +11,6 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
-    path('logout/',LogoutView.as_view(),name='logout'),
-    path('verify/<str:email>/<uuid:code>/',EmailVerificationView.as_view(),name='email_verification'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('verify/<str:email>/<uuid:code>/', EmailVerificationView.as_view(), name='email_verification'),
 ]
