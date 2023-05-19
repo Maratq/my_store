ae4fc23051c6331b3b3284bac29721be2f3684d7
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-19 21:28:28 +0300

    Update settings.py

--- store/store/settings.py
+++ store/store/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG") == 'true'
 
 ALLOWED_HOSTS = ['*']
 
@@ -142,12 +142,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# if DEBUG:
-#    STATICFILES_DIRS = [
-#        BASE_DIR / 'static'
-#    ]
-
-STATIC_ROOT = BASE_DIR / 'static'
+if DEBUG:
+    STATICFILES_DIRS = [
+       BASE_DIR / 'static'
+   ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
