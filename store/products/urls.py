5a4d6ec5b54a799bdd89412b4e97066c6f6091f2
Marat <104265985+Maratq@users.noreply.github.com>
2023-05-19 19:57:14 +0300

    separation

--- store/products/urls.py
+++ store/products/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', ProductsListView.as_view(), name='index'),
     path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
     path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
+
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
